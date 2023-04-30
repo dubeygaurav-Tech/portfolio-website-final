9f2f089b66424469ab6d19dd88f8618dd28e39a6
dubeygaurav-Tech <101603358+dubeygaurav-Tech@users.noreply.github.com>
2023-04-30 22:40:12 +0200

    Update SendEmail.js

--- src/components/SendEmail.js
+++ src/components/SendEmail.js
@@ -1,12 +1,15 @@
-import emailjs from "emailjs-com";
+// import emailjs from "emailjs-com";
 
-export default function sendEmail(templateParams) {
-    emailjs.send('service_gd_gmail', 'template_email_gd', templateParams, '38RJhyjc4R9AeKNxQ')
-      .then((response) => {
-        console.log('Email successfully sent!')
-        alert("Mail sent successfully");
-      }, (err) => {
-        console.error('Failed to send email:', err)
-        alert(err);
-      });
+// export default function sendEmail(templateParams) {
+//     emailjs.send('service_gd_gmail', 'template_email_gd', templateParams, '38RJhyjc4R9AeKNxQ')
+//       .then((response) => {
+//         console.log('Email successfully sent!')
+//         alert("Mail sent successfully");
+//       }, (err) => {
+//         console.error('Failed to send email:', err)
+//         alert(err);
+//       });
+// }
+export default  function sendEmail(templateParams){
+    alert("Mail sent successfully");
 }
